45268b4640672f01e926e6ffc6668b2898c20968
Marinda Keller <mkeller@byupatheway.edu>
2025-07-04 20:36:09 -0600

    working on lastmod date

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,13 +1,7 @@
-// select the DOM elements for output
-
 const year = document.querySelector("#year");
-const full = document.querySelector("#full");
-const worldfull = document.querySelector("#world-full");
-
-// use the date object
 const today = new Date();
 
 
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
-alert(document.lastModified);
+let lastModified = new Date(document.lastModified);
