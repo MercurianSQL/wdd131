ed475269e1542a0d645c4f050ef66db6fab150c7
Marinda Keller <mkeller@byupatheway.edu>
2025-07-13 14:32:30 -0600

    time to debug

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,24 +2,33 @@ const userInput = document.querySelector("#user-input");
 const addButton = document.querySelector("#lg-button");
 const listTen = document.querySelector("#chapter-list");
 
+userInput.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+        addButton.click(); // Triggers the button’s click event
+    }
+});
+
 addButton.addEventListener("click", function () {
     if (userInput.value.trim() !== "") {
         const lineItem = document.createElement("li");
         lineItem.textContent = userInput.value.trim();
         listTen.append(lineItem);
+
         const deleteButton = document.createElement("button");
         deleteButton.textContent = "X";
         deleteButton.style.color = "red";
         deleteButton.style.fontSize = "1.2rem";
-        deleteButton.arialabel = "Delete Entry";
+        deleteButton.setAttribute("aria-label", "Delete Entry");
         lineItem.append(deleteButton);
-        userInput.value = ""; // Clears the input field
-        userInput.focus(); // UX: cursor remains in the <input> field, else has to be clicked again
+
+        deleteButton.addEventListener("click", function () {
+            listTen.removeChild(lineItem);
+            userInput.focus(); // UX: cursor remains in the <input> field, else has to be clicked again
+            userInput.value = ""; // Clears the input field
+            userInput.focus();
+        });
+    } else {
+        userInput.focus();
     }
 });
 
-deleteButton.addEventListener("click", function () {
-
-    listTen.removeChild(lineItem);
-    userInput.focus(); // UX: cursor remains in the <input> field, else has to be clicked again
-})
