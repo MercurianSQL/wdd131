2315b58718fbdeb63910b9d7252816c7a651c74f
Marinda Keller <mkeller@byupatheway.edu>
2025-07-13 15:25:32 -0600

    side by side check with shopping

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,6 +1,6 @@
 const userInput = document.querySelector("#user-input");
 const addButton = document.querySelector("#lg-button");
-const listTen = document.querySelector("#chapter-list");
+const chapterList = document.querySelector("#chapter-list");
 
 // userInput.addEventListener("keyup", function (event) {
 //     if (event.key === "Enter") {
@@ -9,26 +9,25 @@ const listTen = document.querySelector("#chapter-list");
 // });
 
 addButton.addEventListener("click", function () {
-    if (userInput.value.trim() !== "") {
-        const lineItem = document.createElement("li");
-        lineItem.textContent = userInput.value.trim();
-        listTen.append(lineItem);
+    let trImput = userInput.value.trim();
+    if (trImput !== "") {
+        let lineItem = document.createElement("li");
+        lineItem.textContent = trImput;
+        chapterList.appendChild(lineItem);
+        userInput.value = "";
+        userInput.focus();
 
         const deleteButton = document.createElement("button");
         deleteButton.textContent = "X";
-        deleteButton.style.color = "red";
-        deleteButton.style.fontSize = "1.2rem";
-        deleteButton.setAttribute("aria-label", "Delete Entry");
-        lineItem.append(deleteButton);
+        // deleteButton.style.color = "red";
+        // deleteButton.style.fontSize = "1.2rem";
+        // deleteButton.setAttribute("aria-label", "Delete Entry");
+        lineItem.appendChild(deleteButton);
 
         deleteButton.addEventListener("click", function () {
-            listTen.removeChild(lineItem);
-            userInput.focus(); // UX: cursor remains in the <input> field, else has to be clicked again
-            userInput.value = ""; // Clears the input field
+            chapterList.removeChild(lineItem);
             userInput.focus();
         });
-    } else {
-        userInput.focus();
     }
 });
 
