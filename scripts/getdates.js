fb6f61a35da19ddeecf5809b634418193b7f07e1
Marinda Keller <mkeller@byupatheway.edu>
2026-09-04 09:54:50 -0600

    audit report corrections

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+<script>
+    document.getElementById("current year").textContent = new Date().getFullYear();
+</script>
+      <script>
+        document.getElementById("last modified").textContent = new Date().toLocaleDateString();
+      </script>
